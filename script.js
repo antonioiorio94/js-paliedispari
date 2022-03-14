aa1cbd4a710109f99d6e3e4a6f6ea8550f9e4fb1
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-14 14:30:31 +0100

    aggiunta divisione con i metodi split e reverse

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Js è OK!')
+/*
+----------------Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma.*/
+
+//Chiedo una parola all'utente
+
+const parolaUtente = prompt('Inserisci una parola');
+
+//Scomporre la parola in singole unità e inserirla in un array con il metodo split
+
+let parolaDivisa = parolaUtente.split("");
+console.log(parolaDivisa);
+
+// invertire l'array con il metodo reverse
+parolaAlContrario = parolaDivisa.reverse();
+
+
+console.log(parolaAlContrario);
