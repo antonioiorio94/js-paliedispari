8d79a74fbf9a9c1994ac2a9d08e0696f17f7df1d
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-14 14:50:25 +0100

    implementazione funzione

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+//               -------------ESERCIO PALINDROMA------------
+
+//               ------- v1.0 senza funzione
 /*
-----------------Palidroma
+
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma.*/
 
 //Chiedo una parola all'utente
 
+/*
 const parolaUtente = prompt('Inserisci una parola');
 
 //Scomporre la parola in singole unità e inserirla in un array con il metodo split
@@ -24,6 +28,37 @@ console.log(parolaAlContrario);
 //verifica delle parole
 
 if(parolaUtente === parolaAlContrario){
+  console.log(`La parola ${parolaUtente} è un palindromo`);
+}else{
+  console.log(`La parola ${parolaUtente} non è un palindromo`);
+}
+
+
+*/
+
+
+
+      //------------------- v1.1  ESERCIZIO CON FUNZIONE --------------
+
+const parolaUtente = prompt('Inserisci una parola');
+let verifica = verificaPalindromo(parolaUtente);
+
+//creazione funzione
+
+function verificaPalindromo (stringa){
+  let parolaDivisa = stringa.split("");
+  console.log(parolaDivisa);  
+  parolaDivisa = parolaDivisa.reverse();
+  console.log(parolaDivisa);
+  const parolaAlContrario = parolaDivisa.join('');
+  return(parolaAlContrario);
+}
+
+console.log(verifica);
+
+//verifica delle parole
+
+if(parolaUtente === verifica){
   console.log(`La parola ${parolaUtente} è un palindromo`);
 }else{
   console.log(`La parola ${parolaUtente} non è un palindromo`);
